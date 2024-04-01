0011846d39a073aa376b940ff40fef963064566d
Sarahenia20 <sarah.hania15@gmail.com>
2024-04-01 01:57:36 +0100

    Add Class Schema

--- src/entities/Offer.java
+++ src/entities/Offer.java
@@ -1,8 +1,110 @@
 package entities;
+
 import java.util.Date;
-import java.util.Collection;
-import java.util.ArrayList;
+import java.util.List;
 
 public class Offer {
+    private Integer id;
+    private String title;
+    private String description;
+    private String author;
+    private Date createdAt;
+    private Motive motive;
+    private Type type;
+    private Location location;
+    private Status status;
+    private String fileName;
+    private List<Skill> skills;
+    // Constructor
+    public Offer() {
+    }
+    // Getters and setters
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Motive getMotive() {
+        return motive;
+    }
+
+    public void setMotive(Motive motive) {
+        this.motive = motive;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 }
+
+
