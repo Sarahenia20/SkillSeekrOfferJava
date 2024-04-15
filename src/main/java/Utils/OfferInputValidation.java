b9230bfac067b7ec7b1450ccdea175987ce51c57
Sarahenia20 <sarah.hania15@gmail.com>
2024-04-15 06:33:12 +0100

    Offer input validation + email Api

---
+++ src/main/java/Utils/OfferInputValidation.java
@@ -0,0 +1,69 @@
+package Utils;
+
+import Entities.Offers.Offer;
+import Entities.Offers.Skill;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import java.util.Date;
+import java.util.List;
+
+public class OfferInputValidation {
+
+    // Validate all fields except file: Not null
+    public static String validateAllFieldsNotNull(Offer offer) {
+        if (offer.getTitle() == null) {
+            return "Title cannot be empty";
+        } else if (!validateTitle(offer.getTitle())) {
+            return "Title must be at most 150 characters long";
+        } else if (!validateDescription(offer.getDescription())) {
+            return "Description must be at least 100 characters long";
+        } else if (!validateAuthor(offer.getAuthor())) {
+            return "Invalid author email address";
+        } else if (!validateCreatedAt(offer.getCreated_at())) {
+            return "Creation date cannot be in the past";
+        } else if (offer.getMotive() == null) {
+            return "Motive cannot be empty";
+        } else if (offer.getType() == null) {
+            return "Type cannot be empty";
+        } else if (offer.getLocation() == null) {
+            return "Location cannot be empty";
+        } else if (offer.getStatus() == null) {
+            return "Status cannot be empty";
+        } else if (!validateSkills(offer.getSkills())) {
+            return "At least one skill must be selected";
+        } else {
+            return null; // Validation passed
+        }
+    }
+
+    // Validate title: Max 100 characters long
+    private static boolean validateTitle(String title) {
+        return title.length() <= 150;
+    }
+
+    // Validate description: At least 100 characters long
+    private static boolean validateDescription(String description) {
+        return description.length() >= 100;
+    }
+
+    // Validate author: Should be a valid email address
+    private static boolean validateAuthor(String author) {
+        try {
+            InternetAddress internetAddress = new InternetAddress(author);
+            internetAddress.validate();
+            return true;
+        } catch (AddressException e) {
+            return false;
+        }
+    }
+
+    // Validate createdAt: Should not be in the past
+    private static boolean validateCreatedAt(Date createdAt) {
+        return createdAt.after(new Date());
+    }
+
+    // Validate skills: At least one skill selected
+    private static boolean validateSkills(List<Skill> skills) {
+        return skills != null && !skills.isEmpty();
+    }
+}
